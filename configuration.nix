2e7ebe6e2e362b93a18413490d5ce0a24bf31e36
Daren Thoman <darenthoman1189@gmail.com>
2025-06-16 14:25:31 -0400

    first comment

---
+++ configuration.nix
@@ -0,0 +1,224 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "DJT_DESKTOP"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.daren = {
+    isNormalUser = true;
+    description = "Daren";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+      kdePackages.elisa
+      kdePackages.okular
+      htop
+      fastfetch
+      wineWowPackages.stable
+      thunderbird
+      obsidian
+      audacity
+      vlc
+      makemkv
+      virt-manager
+    ];
+  };
+  programs.firefox = {
+   enable = true;
+   package = pkgs.librewolf;
+   policies = {
+     DisableTelemetry = true;
+     DisableFirefoxStudies = true;
+     Preferences = {
+       "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+       "cookiebanners.service.mode" = 2; # Block cookie banners
+       "privacy.donottrackheader.enabled" = true;
+       "privacy.fingerprintingProtection" = true;
+       "privacy.resistFingerprinting" = true;
+       "privacy.trackingprotection.emailtracking.enabled" = true;
+       "privacy.trackingprotection.enabled" = true;
+       "privacy.trackingprotection.fingerprinting.enabled" = true;
+       "privacy.trackingprotection.socialtracking.enabled" = true;
+     };
+      ExtensionSettings = with builtins;
+        let extension = shortId: uuid: {
+          name = uuid;
+          value = {
+            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+        };
+        in listToAttrs [
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
+          (extension "uaswitcher" "user-agent-switcher@ninetailed.ninja")
+          (extension "darkreader" "addon@darkreader.org")
+          (extension "plasma-integration" "plasma-browser-integration@kde.org")
+          (extension "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" "return-youtube-dislikes")
+          (extension "the-camelizer-price-history-ch" "izer@camelcamelcamel.com")
+
+        ];
+        # To add additional extensions, find it on addons.mozilla.org, find
+        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+        # Then, download the XPI by filling it in to the install_url template, unzip it,
+        # run `jq .browser_specific_settings.gecko.id manifest.json` or
+        # `jq .applications.gecko.id manifest.json` to get the UUID
+      };
+    };
+############  numlock on start up  #############
+  let
+    cfg = config.ncfg.services.numlock-on-tty;
+  in
+  {
+    options.ncfg.services.numlock-on-tty = {
+      enable = lib.mkEnableOption "Enable numlock";
+    };
+
+    config = lib.mkIf cfg.enable {
+      systemd.services.numLockOnTty = {
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          # /run/current-system/sw/bin/setleds -D +num < "$tty";
+          ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
+            for tty in /dev/tty{1..6}; do
+                ${pkgs.kbd}/bin/setleds -D +num < "$tty";
+            done
+          '');
+        };
+      };
+    };
+  }
+###############################################
+
+
+  programs.kdeconnect.enable = true;
+  programs.partition-manager.enable = true;
+
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+
+# Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    onlyoffice-desktopeditors
+    python3
+    lsd
+    docker
+    git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+   #Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
