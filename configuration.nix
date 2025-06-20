d801a43e6eb5d5b3ac59352d7fd5c744ceae9977
Daren Thoman <darenthoman1189@gmail.com>
2025-06-20 17:30:18 -0400

    adding librewolf 🐺 config file

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./packages/librewolf.nix
     ];
 
   # Bootloader.
@@ -100,50 +101,12 @@
       vlc
       makemkv
       virt-manager
-    ];
-  };
-  programs.firefox = {
-   enable = true;
-   package = pkgs.librewolf;
-   policies = {
-     DisableTelemetry = true;
-     DisableFirefoxStudies = true;
-     Preferences = {
-       "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
-       "cookiebanners.service.mode" = 2; # Block cookie banners
-       "privacy.donottrackheader.enabled" = true;
-       "privacy.fingerprintingProtection" = true;
-       "privacy.resistFingerprinting" = true;
-       "privacy.trackingprotection.emailtracking.enabled" = true;
-       "privacy.trackingprotection.enabled" = true;
-       "privacy.trackingprotection.fingerprinting.enabled" = true;
-       "privacy.trackingprotection.socialtracking.enabled" = true;
+      discord
+      filezilla
+      vscode
+
+     ];
      };
-      ExtensionSettings = with builtins;
-        let extension = shortId: uuid: {
-          name = uuid;
-          value = {
-            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
-            installation_mode = "normal_installed";
-          };
-        };
-        in listToAttrs [
-          (extension "ublock-origin" "uBlock0@raymondhill.net")
-          (extension "uaswitcher" "user-agent-switcher@ninetailed.ninja")
-          (extension "darkreader" "addon@darkreader.org")
-          (extension "plasma-integration" "plasma-browser-integration@kde.org")
-          (extension "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" "return-youtube-dislikes")
-          (extension "the-camelizer-price-history-ch" "izer@camelcamelcamel.com")
-
-        ];
-        # To add additional extensions, find it on addons.mozilla.org, find
-        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
-        # Then, download the XPI by filling it in to the install_url template, unzip it,
-        # run `jq .browser_specific_settings.gecko.id manifest.json` or
-        # `jq .applications.gecko.id manifest.json` to get the UUID
-      };
-    };
-
 
   programs.kdeconnect.enable = true;
   programs.partition-manager.enable = true;
@@ -165,6 +128,12 @@
     lsd
     docker
     git
+    python313Packages.pip
+    python313Packages.venvShellHook
+    vscode
+    timeshift
+    tree
+
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
