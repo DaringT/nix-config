65d8f9a590dd18068231436d84aadd211d4928b1
Daren Thoman <darenthoman1189@gmail.com>
2025-06-26 17:19:11 -0400

    Enables Flakes

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,9 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Enable Flakes
+  nix.settings.experimental.features = [ "nix-command" "flakes"]
+
   # Set your time zone.
   time.timeZone = "America/New_York";
 
