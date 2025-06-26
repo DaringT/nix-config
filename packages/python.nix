82bbbb3e6bbfc48d53078c974cd12f840d99dbd0
Daren Thoman <darenthoman1189@gmail.com>
2025-06-26 16:56:49 -0400

    Removed pyutube from python.nix

--- packages/python.nix
+++ packages/python.nix
@@ -2,10 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     python313Packages.pip
     python313Packages.venvShellHook
-    python313Packages.pyutube #for downloading YouTube videos
+    # python313Packages.pyutube #for downloading YouTube videos
     python313Packages.pyudev
     python313Packages.tqdm
     python313Packages.rich
