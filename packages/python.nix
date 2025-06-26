2bf175fdc26b5ddf6e8d6c4594d2539d696de6c9
Daren Thoman <darenthoman1189@gmail.com>
2025-06-26 15:52:39 -0400

    Replaced pytube with pyutube in python.nix.

--- packages/python.nix
+++ packages/python.nix
@@ -5,10 +5,11 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     python313Packages.pip
     python313Packages.venvShellHook
-    python313Packages.pytube #for downloading YouTube videos
+    python313Packages.pyutube #for downloading YouTube videos
     python313Packages.pyudev
     python313Packages.tqdm
     python313Packages.rich
+    python313Packages.requests
 
   ];
 }
