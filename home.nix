c854bdddcd12941c86326f9f1b3cec2d3df0cd62
Daren Thoman <darenthoman1189@gmail.com>
2025-11-11 10:41:54 -0500

    convert_to_mp3.desktop was added testing #2

--- home.nix
+++ home.nix
@@ -12,7 +12,7 @@
     ffmpeg
   ];
 
-  home.file."/home/daren/.local/share/kio/servicemenus/convert_to_mp3.desktop".source = ./convert_to_mp3.desktop;
+  # home.file."/home/daren/.local/share/kio/servicemenus/convert_to_mp3.desktop".source = ./convert_to_mp3.desktop;
 
   # Define the content of the .desktop file in the correct location for KDE/Dolphin Service Menus.
 
@@ -20,6 +20,28 @@
   #   source = ./convert_to_mp3.desktop;
   # };
 
+  home.file."/home/test/.local/share/kio/servicemenus/convert_to_mp3.desktop" = {
+  text = ''
+    [Desktop Entry]
+    Type=Service
+    ServiceTypes=KonqPopupMenu/Plugin
+    MimeType=video/*;audio/*;
+    Icon=audio-mp3
+    Actions=mp3
+    X-KDE-Priority=TopLevel
+    X-KDE-StartupNotify=false
+    X-KDE-Submenu=Convert
+
+
+    [Desktop Action mp3]
+    Name=Convert to mp3
+    Icon=audio-mp3
+    TryExec=ffmpeg
+    # Exec=file=%f; ffmpeg -i %f "${file%.*}.mp3"
+    Exec=file=%f; ffmpeg -i %f -q:a 0 -n "${file%.*}.mp3"
+  '';
+  };
+  
   home.sessionVariables = {
   
   };
