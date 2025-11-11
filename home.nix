33379d74d06f1b97d021284af3854e3de60fd8a0
Daren Thoman <darenthoman1189@gmail.com>
2025-11-11 11:05:08 -0500

    convert_to_mp3.desktop was added testing #8

--- home.nix
+++ home.nix
@@ -22,23 +22,23 @@
 
   home.file."/home/daren/.local/share/kio/servicemenus/convert_to_mp3.desktop" = {
     text = ''
-      [Desktop Entry]
-      Type=Service
-      ServiceTypes=KonqPopupMenu/Plugin
-      MimeType=video/*;audio/*;
-      Icon=audio-mp3
-      Actions=mp3
-      X-KDE-Priority=TopLevel
-      X-KDE-StartupNotify=false
-      X-KDE-Submenu=Convert
+    [Desktop Entry]
+    Type=Service
+    ServiceTypes=KonqPopupMenu/Plugin
+    MimeType=video/*;audio/*;
+    Icon=audio-mp3
+    Actions=mp3
+    X-KDE-Priority=TopLevel
+    X-KDE-StartupNotify=false
+    X-KDE-Submenu=Convert
 
 
-      [Desktop Action mp3]
-      Name=Convert to mp3
-      Icon=audio-mp3
-      TryExec=ffmpeg
-      # Exec=file=%f; ffmpeg -i %f \"$\{file%.*}.mp3\"
-      Exec=file=%f; ffmpeg -i %f -q:a 0 -n \"$\{file%.*}.mp3\"
+    [Desktop Action mp3]
+    Name=Convert to mp3
+    Icon=audio-mp3
+    TryExec=ffmpeg
+    # Exec=file=%f; ffmpeg -i %f "''${file%.*}.mp3"
+    Exec=file=%f; ffmpeg -i %f -q:a 0 -n "''${file%.*}.mp3"
     '';
     enable = true;
     executable = true;
