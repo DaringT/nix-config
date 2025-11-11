d50f65d611e207749121ef382edb7007d4083b1f
Daren Thoman <darenthoman1189@gmail.com>
2025-11-11 10:45:17 -0500

    convert_to_mp3.desktop was added testing #4

--- home.nix
+++ home.nix
@@ -37,8 +37,8 @@ home.file."/home/test/.local/share/kio/servicemenus/convert_to_mp3.desktop" = {
     Name=Convert to mp3
     Icon=audio-mp3
     TryExec=ffmpeg
-    # Exec=file=%f; ffmpeg -i %f \"${file%.*}.mp3\"  <-- Escaped double quotes
-    Exec=file=%f; ffmpeg -i %f -q:a 0 -n \"${file%.*}.mp3\" <-- Escaped double quotes
+    # Exec=file=%f; ffmpeg -i %f \"${file%.*}.mp3\"
+    Exec=file=%f; ffmpeg -i %f -q:a 0 -n \"${file%.*}.mp3\"
   '';
   };
 
