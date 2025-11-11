e0966f387a72563128cfc361c7d09a575264c7ed
Daren Thoman <darenthoman1189@gmail.com>
2025-11-11 10:46:23 -0500

    convert_to_mp3.desktop was added testing #4

--- home.nix
+++ home.nix
@@ -20,26 +20,26 @@
   #   source = ./convert_to_mp3.desktop;
   # };
 
-home.file."/home/test/.local/share/kio/servicemenus/convert_to_mp3.desktop" = {
-  text = ''
-    [Desktop Entry]
-    Type=Service
-    ServiceTypes=KonqPopupMenu/Plugin
-    MimeType=video/*;audio/*;
-    Icon=audio-mp3
-    Actions=mp3
-    X-KDE-Priority=TopLevel
-    X-KDE-StartupNotify=false
-    X-KDE-Submenu=Convert
+  home.file."/home/test/.local/share/kio/servicemenus/convert_to_mp3.desktop" = {
+    text = ''
+      [Desktop Entry]
+      Type=Service
+      ServiceTypes=KonqPopupMenu/Plugin
+      MimeType=video/*;audio/*;
+      Icon=audio-mp3
+      Actions=mp3
+      X-KDE-Priority=TopLevel
+      X-KDE-StartupNotify=false
+      X-KDE-Submenu=Convert
 
 
-    [Desktop Action mp3]
-    Name=Convert to mp3
-    Icon=audio-mp3
-    TryExec=ffmpeg
-    # Exec=file=%f; ffmpeg -i %f \"${file%.*}.mp3\"
-    Exec=file=%f; ffmpeg -i %f -q:a 0 -n \"${file%.*}.mp3\"
-  '';
+      [Desktop Action mp3]
+      Name=Convert to mp3
+      Icon=audio-mp3
+      TryExec=ffmpeg
+      # Exec=file=%f; ffmpeg -i %f \"${file%.*}.mp3\"
+      Exec=file=%f; ffmpeg -i %f -q:a 0 -n \"${file%.*}.mp3\"
+    '';
   };
 
   home.sessionVariables = {
