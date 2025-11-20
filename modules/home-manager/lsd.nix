ebe989d4ed8f2db56514a423d6517326428c4611
Daren Thoman <darenthoman1189@gmail.com>
2025-11-19 21:14:14 -0500

    added lsd to home-mgr #6

--- modules/home-manager/lsd.nix
+++ modules/home-manager/lsd.nix
@@ -21,49 +21,65 @@ in {
   };
 
   home.file.".config/lsd/colors.yaml".source = yamlFormat.generate "colors.yaml" {
-    user = "dark_yellow";
-    group = "dark_yellow";
-    permission = {
-      read = "dark_yellow";
-      write = "dark_magenta";
-      exec = "dark_red";
-      exec-sticky = "dark_blue";
-      no-access = "red";
-      octal = "dark_cyan";
-      acl = "dark_cyan";
-      context = "dark_cyan";
+    file-types = {
+      # Directories
+      directory = "blue bold";
+
+      # Executable files
+      executable = "green bold";
+
+      # Symlinks (ln)
+      symlink = "cyan bold";
+
+      # Specific extension: Markdown files
+      ".md" = "magenta";
+      
+      # Specific extension: Archive files
+      ".zip" = "red bold";
     };
-    date = {
-      hour-old = "dark_cyan";
-      day-old = "dark_cyan";
-      older = "dark_cyan";
-    };
-    size = {
-      none = "dark_green";
-      small = "dark_green";
-      medium = "dark_green";
-      large = "dark_green";
-    };
-    inode = {
-      valid = "dark_magenta";
-      invalid = "red";
-    };
-    links = {
-      valid = "dark_blue";
-      invalid = "dark_red";
-    };
-    tree-edge = "dark_cyan";
-    git-status = {
-      default = "dark_cyan";
-      unmodified = "dark_cyan";
-      ignored = "dark_cyan";
-      new-in-index = "dark_green";
-      new-in-workdir = "dark_green";
-      typechange = "dark_yellow";
-      deleted = "dark_red";
-      renamed = "dark_green";
-      modified = "dark_yellow";
-      conflicted = "dark_red";
+    # user = "dark_yellow";
+    # group = "dark_yellow";
+    # permission = {
+    #   read = "dark_yellow";
+    #   write = "dark_magenta";
+    #   exec = "dark_red";
+    #   exec-sticky = "dark_blue";
+    #   no-access = "red";
+    #   octal = "dark_cyan";
+    #   acl = "dark_cyan";
+    #   context = "dark_cyan";
+    # };
+    # date = {
+    #   hour-old = "dark_cyan";
+    #   day-old = "dark_cyan";
+    #   older = "dark_cyan";
+    # };
+    # size = {
+    #   none = "dark_green";
+    #   small = "dark_green";
+    #   medium = "dark_green";
+    #   large = "dark_green";
+    # };
+    # inode = {
+    #   valid = "dark_magenta";
+    #   invalid = "red";
+    # };
+    # links = {
+    #   valid = "dark_blue";
+    #   invalid = "dark_red";
+    # };
+    # tree-edge = "dark_cyan";
+    # git-status = {
+    #   default = "dark_cyan";
+    #   unmodified = "dark_cyan";
+    #   ignored = "dark_cyan";
+    #   new-in-index = "dark_green";
+    #   new-in-workdir = "dark_green";
+    #   typechange = "dark_yellow";
+    #   deleted = "dark_red";
+    #   renamed = "dark_green";
+    #   modified = "dark_yellow";
+    #   conflicted = "dark_red";
     };
   };
 }
