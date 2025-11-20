82b96124381bd13ad9065741c0e7f7f8f6d4049c
Daren Thoman <darenthoman1189@gmail.com>
2025-11-19 20:54:02 -0500

    added lsd to home-mgr

--- configuration.nix
+++ configuration.nix
@@ -105,7 +105,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     python3
-    lsd # lsd can be used as tree with lsd --tree.
+    # lsd # lsd can be used as tree with lsd --tree.
     docker
     git
     fastfetch
---
+++ modules/home-manager/lsd.nix
@@ -0,0 +1,27 @@
+programs.lsd.colors = {
+    # This directly sets the colors for the icon and file name
+    file-types = {
+      # Directories (di)
+      directory = "blue bold";
+
+      # Executable files (ex)
+      # executable = "green bold";
+
+      # Symlinks (ln)
+      # symlink = "cyan bold";
+
+      # Specific extension: Markdown files
+      ".md" = "magenta";
+      ".py" = "yellow bold";
+      
+      # Specific extension: Archive files
+      # ".zip" = "red bold";
+    };
+
+    # You can also configure colors for the visible blocks
+    # blocks = {
+    #   permission = "yellow";
+    #   user = "green";
+    #   size = "white";
+    # };
+  };
