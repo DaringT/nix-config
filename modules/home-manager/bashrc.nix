7a33b169fac4ff51181d4e67f883fb443ccad63c
Daren Thoman <darenthoman1189@gmail.com>
2025-11-26 22:12:08 -0500

    Added bashrc.nix #6

--- modules/home-manager/bashrc.nix
+++ modules/home-manager/bashrc.nix
@@ -89,7 +89,6 @@ in
     egrep = "egrep --color=auto";
     
     # # Alert Alias (using multiline string)
-    # alert = ''notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e 's/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//')"'';
   };
 
   # 5. Packages
