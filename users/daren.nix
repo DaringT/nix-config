cda3ab381dfa816d91dcc2d14847f4b8106e4904
Daren Thoman <darenthoman1189@gmail.com>
2025-11-30 13:09:39 -0500

    reenabled fonts.nix and bashrc.nix

--- users/daren.nix
+++ users/daren.nix
@@ -47,8 +47,8 @@
     ../modules/home-manager
     ../modules/wm/kde-plasma/dolphin/dolphin.nix
     ../modules/browsers/librewolf.nix
-    # ../modules/wm/kde-plasma/plasma-panel.nix
-    # ../modules/fonts.nix
+    ../modules/wm/kde-plasma/plasma-panel.nix
+    ../modules/fonts.nix
   ];
   
 
