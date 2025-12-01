cb7f1e3e266f5f2f04926e00200c00a2fd8e9679
Daren Thoman <darenthoman1189@gmail.com>
2025-11-30 23:34:41 -0500

    Using nix new syntacs to git in home-manager #2

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,13 +1,13 @@
 {
   programs.git = {
     enable = true;
-    user = {
-      Name = "DaringT";
-      email = "darenthoman1189@gmail.com";
-    };
     settings = {
       init.defaultBranch = "main";
       core.editor = "nano";
+      user = {
+        Name = "DaringT";
+        email = "darenthoman1189@gmail.com";
+      };
   };
 };
 }
