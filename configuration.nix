f47e68351b0d55824c4010209ea8fb1392080004
Daren Thoman <darenthoman1189@gmail.com>
2026-01-10 10:55:19 -0500

    fixing audio. #3

--- configuration.nix
+++ configuration.nix
@@ -70,18 +70,19 @@
   # Enable sound with pipewire.
   services.pulseaudio.enable = true;
   security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = false;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  
+  services.pipewire = {
+    enable = false;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    If you want to use JACK applications, uncomment this
+    jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  # };
+    use the example session manager (no others are packaged yet so this is enabled by default,
+    no need to redefine it in your config for now)
+    media-session.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
