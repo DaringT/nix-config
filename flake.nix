7a6a4307d65cb14e9f974b55abbfec71d0b81554
Daren Thoman <darenthoman1189@gmail.com>
2026-01-10 16:52:12 -0500

    adding gstreamer #3

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
           ./configuration.nix
           ./modules/pkgs/python.nix
           ./hosts/DJT-DESKTOP/hardware-configuration.nix
-          ./modules/gstreamer.nix
+          # ./modules/gstreamer.nix
           {networking.hostName = "DJT-DESKTOP";}
           home-manager.nixosModules.default # HM integration
         ];
@@ -91,6 +91,22 @@
         ];
       };
     };
+      devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+        gst_all_1.gstreamer
+        # Common plugins like "filesrc" to combine within e.g. gst-launch
+        gst_all_1.gst-plugins-base
+        # Specialized plugins separated by quality
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        # Plugins to reuse ffmpeg to play almost every video format
+        gst_all_1.gst-libav
+        # Support the Video Audio (Hardware) Acceleration API
+        gst_all_1.gst-vaapi
+        #...
+      ];
+    };
 
-  }; # <--- CORRECT CLOSING BRACE for the entire 'outputs' attribute set
-}
+  };
