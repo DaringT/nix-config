c2b5431258ae1b8272c17feb1b4d16617a1ba46c
Daren Thoman <darenthoman1189@gmail.com>
2026-03-30 08:18:28 -0400

    getting Heilum browser added

--- users/daren.nix
+++ users/daren.nix
@@ -45,6 +45,7 @@
     ../modules/home-manager
     ../modules/pkgs/dolphin.nix
     ../modules/pkgs/librewolf.nix
+    ../modules/pkgs/helium-browser.nix
     ../modules/wm/kde-plasma/plasma-panel.nix
 
     # ../modules/fonts.nix
