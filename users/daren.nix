9a64087754443381e5912251206fce56a3b2ce8d
Daren Thoman <darenthoman1189@gmail.com>
2026-03-30 08:46:05 -0400

    getting Heilum browser added #4

--- users/daren.nix
+++ users/daren.nix
@@ -8,36 +8,36 @@
   # programs.bash.enable = true;
   nixpkgs.config.allowUnfree = true;
   # 2. Package Management (replacing the old users.users.daren.packages)
-  home.packages = [
-      pkgs.kdePackages.elisa
-      pkgs.kdePackages.kate
-      pkgs.kdePackages.okular
-      pkgs.kdePackages.qtsvg
-
-      pkgs.pear-desktop
-      pkgs.wineWow64Packages.stable
-      pkgs.thunderbird
-      pkgs.obsidian
-      pkgs.handbrake
-      pkgs.vesktop # discord client
-      pkgs.audacity
-      pkgs.vlc
-      pkgs.mpv
-      pkgs.krename
-      pkgs.makemkv
-      pkgs.virt-manager
-      pkgs.plasma-panel-colorizer
-      pkgs.mkvtoolnix
-      pkgs.remmina
-      pkgs.brave
-      pkgs.spotdl
-      pkgs.yt-dlp
-      pkgs.youtube-tui
-      pkgs.kdePackages.kcalc
-      pkgs.onlyoffice-desktopeditors
-      pkgs.ffmpeg
-      pkgs.eza
-      inputs.helium.packages.${pkgs.system}.default
+  home.packages = with pkgs; [
+      kdePackages.elisa
+      kdePackages.kate
+      kdePackages.okular
+      kdePackages.qtsvg
+      
+      pear-desktop
+      wineWow64Packages.stable
+      thunderbird
+      obsidian
+      handbrake
+      vesktop # discord client
+      audacity
+      vlc
+      mpv
+      krename
+      makemkv
+      virt-manager
+      plasma-panel-colorizer
+      mkvtoolnix
+      remmina
+      brave
+      spotdl
+      yt-dlp
+      youtube-tui
+      kdePackages.kcalc
+      onlyoffice-desktopeditors
+      ffmpeg
+      eza
+      inputs.helium.packages.${system}.default
   ];
 
 
@@ -45,7 +45,7 @@
     ../modules/home-manager
     ../modules/pkgs/dolphin.nix
     ../modules/pkgs/librewolf.nix
-    ../modules/pkgs/helium-browser.nix
+    # ../modules/pkgs/helium-browser.nix
     ../modules/wm/kde-plasma/plasma-panel.nix
 
     # ../modules/fonts.nix
