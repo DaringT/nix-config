d18051a29650f1fad6ce352d0e0e70835ae1e437
Daren Thoman <darenthoman1189@gmail.com>
2026-05-27 14:24:54 -0400

    moved testing zsh starship

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -13,90 +13,56 @@
       ignoreDups = true;
       ignoreSpace = true;
       extended = true;
+
+      initContent = ''
+      bindkey "^[[3~" delete-char
+    '';
     };
-    # initContent = ''
-    #   [ -f ~/.secrets ] && source ~/.secrets
-    # '';
-  };
 
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
+    shellAliases = {
+      # ls/cat replacements
+      lss = lib.mkForce"ls"; # Original 'ls'
+      ls = lib.mkForce"lsd";
+      ll = lib.mkForce"lsd -alF";
+      lll = lib.mkForce"lsd -1";
+      # la = "lsd -A";
+      la = lib.mkForce "lsd -A";
+      l = lib.mkForce"lsd -CF";
+      tree = lib.mkForce"lsd --tree";
+      cat = "bat";
+      iso = "echo dd if=/dev/cdrom of=output.iso";
+      
+      # Directory/Navigation
+      "cd.." = "cd ..";
+      nixos = "cd ~/nix-config";
+      rn = "mv";
 
-  programs.starship = {
-    enable = true;
-    settings = builtins.fromTOML (builtins.readFile ./starship.toml);
-  };
+      # Python
+      python = "python3";
+      py = "python3";
+
+      # Utilities
+      "disc-info" = "lsscsi -g";
+      cls = "clear";
+      ipa = "hostname -I | awk '{print $1}'";
+
+      # External Programs & Nix Management
+      yt = "yt-dlp --cookies yt-cookies.txt -t mp3";
+      hms = "home-manager switch --flake ~/nix-config#daren";
+      nrs = "cd ~/nix-config && sudo nixos-rebuild switch --flake .#DJT-DESKTOP";
+      nit = "cd ~/nix-config && git add -A && git commit -m";
+      
+      # Grep Colors
+      grep = "grep --color=auto";
+      fgrep = "fgrep --color=auto";
+      egrep = "egrep --color=auto";
+      cb = "xclip -sel clip";
+      # # Alert Alias (using multiline string)
+    };
+
+    programs.starship = {
+      enable = true;
+      # settings = builtins.fromTOML (builtins.readFile ./starship.toml);
+      eval "$(starship init zsh)"
+    };
 }
-
-
-#   programs.zsh = {
-#     enable = true;
-    
-#     # With Oh-My-Zsh:
-#     oh-my-zsh = {
-#       enable = true;
-#       plugins = [
-#         "git"         # also requires `programs.git.enable = true;`
-#       ];
-#       theme = "robbyrussell";
-#     };
-
-    
-#     enableCompletion = true;
-#     autosuggestion.enable = true;
-#     syntaxHighlighting.enable = true;
-       
-#     shellAliases = {
-#       # ls/cat replacements
-#       lss = lib.mkForce"ls"; # Original 'ls'
-#       ls = lib.mkForce"lsd";
-#       ll = lib.mkForce"lsd -alF";
-#       lll = lib.mkForce"lsd -1";
-#       # la = "lsd -A";
-#       la = lib.mkForce "lsd -A";
-#       l = lib.mkForce"lsd -CF";
-#       tree = lib.mkForce"lsd --tree";
-#       cat = "bat";
-#       iso = "echo dd if=/dev/cdrom of=output.iso";
-      
-#       # Directory/Navigation
-#       "cd.." = "cd ..";
-#       nixos = "cd ~/nix-config";
-#       rn = "mv";
-
-#       # Python
-#       python = "python3";
-#       py = "python3";
-
-#       # Utilities
-#       "disc-info" = "lsscsi -g";
-#       cls = "clear";
-#       ipa = "hostname -I | awk '{print $1}'";
-
-#       # External Programs & Nix Management
-#       yt = "yt-dlp --cookies yt-cookies.txt -t mp3";
-#       hms = "home-manager switch --flake ~/nix-config#daren";
-#       nrs = "cd ~/nix-config && sudo nixos-rebuild switch --flake .#DJT-DESKTOP";
-#       nit = "cd ~/nix-config && git add -A && git commit -m";
-      
-#       # Grep Colors
-#       grep = "grep --color=auto";
-#       fgrep = "fgrep --color=auto";
-#       egrep = "egrep --color=auto";
-#       cb = "xclip -sel clip";
-#       # # Alert Alias (using multiline string)
-#     };
-
-
-#     # histSize = 10000;
-#     # histFile = "$HOME/.zsh_history";
-#     setOptions = ["HIST_IGNORE_ALL_DUPS"];
-
-#     history.size = 10000;
-#     initContent = ''
-#       bindkey "^[[3~" delete-char
-#     '';
-#   };
-# }
